75fd8f7e775c76346d1a68932bb4b4a6a9150fd4
Sharepoint Admin <spadmin@MS.Local>
2021-04-27 10:03:55 +0430

    Add project files.

---
+++ ASMXinSharePointEr/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="6399ee62-34ed-4b75-b6ab-5a75301870fc" featureId="6399ee62-34ed-4b75-b6ab-5a75301870fc" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="ASMXinSharePointEr Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="52fa8919-2e56-4aed-b334-715d7edbf19d" />
+  </projectItems>
+</feature>
---
+++ ASMXinSharePointEr/UsingCasService/UsingCasService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+using System;
+using System.Security.Permissions;
+using ASMXinSharePointEr.CASService;
+
+namespace ASMXinSharePointEr.UsingCasService
+{
+    /// <summary>
+    /// List Item Events
+    /// </summary>
+    public class UsingCasService : SPItemEventReceiver
+    {
+        private readonly CASService.IntegrationService_UserSoap casService = new 
+            IntegrationService_UserSoapClient();
+        /// <summary>
+        /// An item was added.
+        /// </summary>
+        public override void ItemAdded(SPItemEventProperties properties)
+        {
+        }
+    }
+}
